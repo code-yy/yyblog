350bf7289f0b2d4e7dd86562ab0e6d3e6abb95e8
yuto yoshino <jiyeyouren523@gmail.com>
2021-09-02 22:14:25 +0900

    ♻️ ファイル分けしました

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { FaTwitter } from "react-icons/fa";
-import Header from "../components/Header";
-import Main from "../components/Main";
-import Footer from "../components/Footer";
+import Header from "../components/Header/Header";
+import Main from "../components/Main/Main";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
