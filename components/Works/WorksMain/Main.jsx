48773e3c0041f54f112d5216c14d16489942121a
yuto yoshino <jiyeyouren523@gmail.com>
2021-09-03 23:09:42 +0900

    ✨ コンタクトページと、実績ページを新しく作りました。

---
+++ components/Works/WorksMain/Main.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import styles from "./Main.module.css";
+
+export default function Works_Main() {
+  return (
+    <main className={styles.main}>
+      <h2 className={styles.circle}></h2>
+
+      <div className={styles.grid}>
+        <p className={styles.description}>Works</p>
+
+        {/* <ul>
+          <Link href="/Profile">
+            <a>
+              <li className={styles.list_style1}>Profile</li>
+            </a>
+          </Link>
+          <Link href="/Works">
+            <a>
+              <li className={styles.list_style2}>Works</li>
+            </a>
+          </Link>
+          <Link href="/Contact">
+            <a>
+              <li className={styles.list_style3}>Contact</li>
+            </a>
+          </Link>
+        </ul> */}
+      </div>
+    </main>
+  );
+}
