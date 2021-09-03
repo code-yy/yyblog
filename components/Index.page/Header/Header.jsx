05f1ae233bdc2d00ce2741437f13a94bc947458e
yuto yoshino <jiyeyouren523@gmail.com>
2021-09-03 23:11:44 +0900

    ♻️ Topのリンク追加と、GitHubのリンクも追加しました

--- components/Index.page/Header/Header.jsx
+++ components/Index.page/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.Header}>
-      <Link href="/">
+      <Link href="https://github.com/Utopia300">
         <a>
           <div></div>
         </a>
@@ -12,15 +12,20 @@ export default function Header() {
       <ul>
         <Link href="/">
           <a>
-            <li className={styles.list_style1}>Profile</li>
+            <li className={styles.list_style0}>Top</li>
           </a>
         </Link>
         <Link href="/Profile">
           <a>
+            <li className={styles.list_style1}>Profile</li>
+          </a>
+        </Link>
+        <Link href="/Works">
+          <a>
             <li className={styles.list_style2}>Works</li>
           </a>
         </Link>
-        <Link href="/">
+        <Link href="/Contact">
           <a>
             <li className={styles.list_style3}>Contact</li>
           </a>
