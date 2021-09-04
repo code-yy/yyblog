ea3421778efa26ce8f5b178ce36ba37a5d845224
yuto yoshino <jiyeyouren523@gmail.com>
2021-09-04 23:41:31 +0900

    ♻️ ちょっとだけ変更

--- components/Works/WorksMain/Main.jsx
+++ components/Works/WorksMain/Main.jsx
@@ -5,20 +5,20 @@ export default function Works_Main() {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-        <p className={styles.description}>Works</p>
+        <p className={styles.description}>Portfolio</p>
 
         <div className={styles.grid_div}>
           <Link href="https://utopiayy.sakura.ne.jp/">
-            <a className={styles.list_style1}></a>
+            <a className={styles.list_style1} width="200px"></a>
           </Link>
           <Link href="https://utopia300.github.io/yuto_it/todo.test/index.html">
-            <a className={styles.list_style2}></a>
+            <a className={styles.list_style2} width="200px"></a>
           </Link>
           <Link href="https://utopia300.github.io/yuto_it/quiz.test/index.html">
-            <a className={styles.list_style3}></a>
+            <a className={styles.list_style3} width="200px"></a>
           </Link>
           <Link href="https://yuto-page.vercel.app/">
-            <a className={styles.list_style4}></a>
+            <a className={styles.list_style4} width="200px"></a>
           </Link>
         </div>
       </div>
