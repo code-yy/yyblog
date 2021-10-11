4c04c71c75d5e417359219224a868bc39109c609
ユウト <jiyeyouren523@gmail.com>
2021-10-11 16:23:36 +0900

    ♻️ 不要な画像のスペースを削除しました

--- src/components/Works/WorksMain/Main.jsx
+++ src/components/Works/WorksMain/Main.jsx
@@ -2,9 +2,6 @@ import Link from "next/link";
 import styles from "./Main.module.css";
 
 export default function Works_Main() {
-  const click = () => {
-    alert("こんにちは");
-  };
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
@@ -13,14 +10,6 @@ export default function Works_Main() {
         <div className={styles.grid_div}>
           <Link href="https://utopiayy.sakura.ne.jp/">
             <a className={styles.list_style1} width="200px"></a>
-          </Link>
-
-          <Link href="https://utopia300.github.io/yuto_it/todo.test/index.html">
-            <a className={styles.list_style2} width="200px"></a>
-          </Link>
-
-          <Link href="https://utopia300.github.io/yuto_it/quiz.test/index.html">
-            <a className={styles.list_style3} width="200px"></a>
           </Link>
 
           <Link href="https://yuto-page.vercel.app/">
