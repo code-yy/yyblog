0e29f26107d74df5e6810362aded7349ad25fee3
ユウト <jiyeyouren523@gmail.com>
2021-10-14 20:03:55 +0900

    ✨ デザインを大きく変更し、blogページの追加を行いました。

--- src/pages/blogs/[id]/index.tsx
+++ src/pages/blogs/[id]/index.tsx
@@ -1,11 +1,8 @@
 import { client } from "../../../../lib/client";
-import Link from "next/link";
 import type { NextPage } from "next";
 import Header from "../../../components/Index.page/Header/Header";
 import Footer from "../../../components/Index.page/Footer/Footer";
 import styles from "./blog.module.css";
-import { FaGithub, FaTwitter } from "react-icons/fa";
-import { GrInstagram } from "react-icons/gr";
 
 type Props = {
   blogs: any;
@@ -14,52 +11,23 @@ type Props = {
 const BlogId: NextPage<Props> = (props) => {
   return (
     <div className={styles.container}>
-      <Header title="Blog || YutoBlog" />
+      <Header title={`${props.blogs.title} || YutoBlog`} />
       <main>
         {/* blog */}
         <div className={styles.wrapper}>
           <div className={styles.blogs}>
             <h1 className={styles.title}>{props.blogs.title}</h1>
-            <img src={props.blogs.image.url} alt="" width={700} />
-            <div>
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: props.blogs.body,
-                }}
-              />
-            </div>
-          </div>
-
-          {/* profile */}
-          <div className={styles.profile}>
-            <div>
-              <div className={styles.profile_img}></div>
-              <div className={styles.profile_icons}>
-                <Link href="https://twitter.com/yuto_yy_76">
-                  <a className={styles.profile_icon}>
-                    <FaTwitter size={30} color={"#00acee"} />
-                  </a>
-                </Link>
-                <Link href="https://github.com/Utopia300">
-                  <a className={styles.profile_icon}>
-                    <FaGithub size={30} />
-                  </a>
-                </Link>
-                <Link href="https://www.instagram.com/yuto.jsx/?hl=ja">
-                  <a className={styles.profile_icon}>
-                    <GrInstagram size={30} color={"red"} />
-                  </a>
-                </Link>
+            <div className={styles.body}>
+              <img src={props.blogs.image.url} alt="" className={styles.blog_image} />
+              <div className={styles.blog_container}>
+                <div
+                  className={styles.blog_body}
+                  dangerouslySetInnerHTML={{
+                    __html: props.blogs.body,
+                  }}
+                />
               </div>
             </div>
-            <div className={styles.profile_name}>ユウト</div>
-            <p className={styles.profile_yuto}>
-              兵庫県在住、フロントエンド志望の19歳です。
-              <br />
-              プログラミング・フットボール・音楽・アニメ・ゲームが大好き。
-              <br />
-            </p>
-            <button className={styles.profile_button}>プロフィール</button>
           </div>
         </div>
       </main>
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,9 +1,10 @@
-import styles from "../../styles/Home.module.css";
+import styles from "./blog.module.css";
 import { client } from "../../../lib/client";
 import Link from "next/link";
 import type { GetStaticProps, NextPage } from "next";
 import Header from "../../components/Index.page/Header/Header";
 import Footer from "../../components/Index.page/Footer/Footer";
+import { fixDateFormat } from "../../../lib/fixDateFormat";
 
 export const getStaticProps: GetStaticProps = async () => {
   const blogs = await client.get({ endpoint: "blogs" });
@@ -24,17 +25,27 @@ const Blog: NextPage<Props> = (props) => {
     <div className={styles.container}>
       <Header title="Blog || YutoBlog" />
 
-      <ul>
-        {props.blogs.contents.map((blogs: any, index: any) => {
-          return (
-            <li key={index}>
+      {props.blogs.contents.map((blogs: any, index: any, tags: any) => {
+        return (
+          <div key={index} className={styles.wrap}>
+            <div className={styles.blogs}>
+              {/* タイトルを表示 */}
               <Link href={`/blogs/${blogs.id}`}>
-                <a>{blogs.title}</a>
+                <a>
+                  <h2 className={styles.blogs_title}>{blogs.title}</h2>
+                </a>
               </Link>
-            </li>
-          );
-        })}
-      </ul>
+
+              {/* 日付を表示 */}
+              <span className="block">
+                {fixDateFormat(blogs.createdAt)}
+                <br />
+                {/* タグを表示 */}#{blogs.category[0]} #{blogs.category[1]} #{blogs.category[2]}
+              </span>
+            </div>
+          </div>
+        );
+      })}
       <Footer />
     </div>
   );
