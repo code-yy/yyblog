35fc4d7f13a7c265006bd897d99cb6c27aa37154
ユウト <jiyeyouren523@gmail.com>
2021-10-17 14:08:29 +0900

    🎨 フッターのtwitter、メールを削除し、ナビゲーションをつけました

--- src/components/Index.page/Footer/Footer.jsx
+++ src/components/Index.page/Footer/Footer.jsx
@@ -1,23 +1,34 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
-import { FaTwitter } from "react-icons/fa";
-import { IoMdMail } from "react-icons/io";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h3 className={styles.logo}>
-        <Link href="https://twitter.com/yuto_yy_76">
+      <ul>
+        {/* top */}
+        <Link href="/">
           <a>
-            <FaTwitter size={30} color={"#00acee"} />
+            <li className={styles.list_style0}>Top</li>
           </a>
         </Link>
-        <p className={styles.logo_p}>yuto_yy_76</p>
-      </h3>
-      <h3 className={styles.logo_2}>
-        <IoMdMail size={30} />
-        <p className={styles.logo_p}>jiyeyouren523@gmail.com</p>
-      </h3>
+        {/* Profile */}
+        <Link href="/Profile">
+          <a>
+            <li className={styles.list_style1}>Profile</li>
+          </a>
+        </Link>
+        {/* Works */}
+        <Link href="/Portfolio">
+          <a>
+            <li className={styles.list_style2}>Portfolio</li>
+          </a>
+        </Link>
+        <Link href="/blogs">
+          <a>
+            <li className={styles.list_style2}>Blog</li>
+          </a>
+        </Link>
+      </ul>
     </footer>
   );
 }
