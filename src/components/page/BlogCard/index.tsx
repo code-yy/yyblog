e2fece88830a72c2ad6c80a3833a6eb4b879d717
Utopia300 <jiyeyouren523@gmail.com>
2021-12-25 22:44:52 +0900

    ♻️ BlogCardコンポーネントを作成

---
+++ src/components/page/BlogCard/index.tsx
@@ -0,0 +1,34 @@
+import { VFC } from "react";
+import Link from "next/link";
+import { fixDateFormat } from "src/lib/fixDateFormat";
+
+type Props = {
+  blogs: {
+    id: string;
+    title: string;
+    description: string;
+    image: {
+      url: string;
+    };
+    updatedAt: string;
+  };
+};
+
+export const BlogCard: VFC<Props> = (props) => {
+  return (
+    <Link href={`/blogs/${props.blogs.id}`}>
+      <a className="block md:flex md:justify-center items-center py-4 w-full">
+        <img
+          src={props.blogs.image.url}
+          alt={props.blogs.image.url}
+          className="block object-cover w-full md:w-1/6 min-h-hull border"
+        />
+        <div className="md:pl-4 w-full md:w-3/4">
+          <div>{fixDateFormat(props.blogs.updatedAt)}</div>
+          <h3 className="text-lg font-bold">{props.blogs.title}</h3>
+          <p className="text-sm text-gray-700">{props.blogs.description}</p>
+        </div>
+      </a>
+    </Link>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { Blogs } from "src/types/types";
-import { fixDateFormat } from "src/lib/fixDateFormat";
 import { client } from "src/lib/client";
 import { Headline } from "src/components/model/Headline";
+import { BlogCard } from "src/components/page/BlogCard";
 
 export const getStaticProps: GetStaticProps = async () => {
   const blogs: Blogs = await client.get({ endpoint: "blogs" });
@@ -31,20 +30,7 @@ const Home: NextPage<Props> = (props) => {
       {props.blogs.contents.map((blogs: any, index: any) => {
         return (
           <div key={index} className="mb-4 rounded-md border hover:shadow-sm transition bg-gray-50">
-            <Link href={`/blogs/${blogs.id}`}>
-              <a className="block md:flex md:justify-center items-center py-4 w-full">
-                <img
-                  src={blogs.image.url}
-                  alt={blogs.image.url}
-                  className="block object-cover w-full md:w-1/6 min-h-hull border"
-                />
-                <div className="md:pl-4 w-full md:w-3/4">
-                  <div>{fixDateFormat(blogs.updatedAt)}</div>
-                  <h3 className="text-lg font-bold">{blogs.title}</h3>
-                  <p className="text-sm text-gray-700">{blogs.description}</p>
-                </div>
-              </a>
-            </Link>
+            <BlogCard blogs={blogs} />
           </div>
         );
       })}
